4f00621cc6a5360639294daa1367d88da634fc72
fapdev77 <fapdev77@gmail.com>
2023-05-13 18:11:55 -0300

    Implementar H2 Server e conexao com DBeaver

--- src/main/java/com/bckndjsb/prj0001/Prj0001Application.java
+++ src/main/java/com/bckndjsb/prj0001/Prj0001Application.java
@@ -1,7 +1,11 @@
 package com.bckndjsb.prj0001;
 
+import java.sql.SQLException;
+
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Prj0001Application {
@@ -9,5 +13,12 @@ public class Prj0001Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Prj0001Application.class, args);
 	}
-
+	
+	//aqui fiz uma melhoria no codigo permitindo que rode um servidor h2 e disponibilizar a conexão 
+	//atraves de um servidor na porta 9092 para usar com outros programas tal como DBeaver 
+	//para esse projeto usamos o mem:testdb como database 
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    Server h2Server() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
+    }
 }
