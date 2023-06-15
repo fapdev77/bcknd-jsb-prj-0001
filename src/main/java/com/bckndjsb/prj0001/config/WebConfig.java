2b16181ca98c473315fa1ca995a359ecbf16a7a9
fapdev77 <fapdev77@gmail.com>
2023-06-15 16:23:50 -0300

    Incluido logger na classe para imprimir a configuracao

--- src/main/java/com/bckndjsb/prj0001/config/WebConfig.java
+++ src/main/java/com/bckndjsb/prj0001/config/WebConfig.java
@@ -1,5 +1,7 @@
 package com.bckndjsb.prj0001.config;
 
+import java.util.logging.Logger;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,12 +10,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
+    private static final Logger logger = Logger.getLogger(WebConfig.class.getName());
 
 	@Value("${cors.origins}")
 	private String corsOrigins;
 
     @Bean
     WebMvcConfigurer corsConfigurer() {
+    	//Exibe as configurações
+        logger.info("cors.origins: " + corsOrigins);
+
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
